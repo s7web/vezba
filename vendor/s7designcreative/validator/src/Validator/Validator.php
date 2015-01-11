5d32065d7f2e95cc016005b25c83e12382f0d8da
s7npaic <npaic@s7designcreative.com>
2015-01-11 19:38:01 +0100

    added validation methods

--- vendor/s7designcreative/validator/src/Validator/Validator.php
+++ vendor/s7designcreative/validator/src/Validator/Validator.php
@@ -1,14 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nenadpaic
- * Date: 1/11/2015
- * Time: 5:55 PM
- */
-
 namespace Validator;
 
-
+/**
+ * Class Validator
+ * @package Validator
+ *
+ * @version 11.01.2015
+ * @author s7designcreative
+ */
 class Validator {
-
+    /**
+     * Run the validator
+     *
+     * @param array $input
+     * @param array $rules
+     * @return ValidatorType
+     */
+    public static function make($input, $rules){
+        return new ValidatorType( $input, $rules);
+    }
 } 
