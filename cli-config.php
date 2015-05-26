4f3de3e7e70973fe2f21777e28da1a2defc71614
s7npaic <npaic@s7designcreative.com>
2015-05-26 09:24:23 +0200

    changed cli-config

--- cli-config.php
+++ cli-config.php
@@ -1,4 +1,21 @@
 <?php
 require_once 'app/init.php';
 
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$paths     = array( SITE_PATH.'/src' );
+$isDevMode = false;
+
+$dbParams = array(
+    'host'     => DATABASE_HOST,
+    'driver'   => DATABASE_DRIVER,
+    'user'     => DATABASE_USERNAME,
+    'password' => DATABASE_PASSWORD,
+    'dbname'   => DATABASE_NAME,
+);
+
+$config        = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
+
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
