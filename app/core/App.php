965b5fa073f977ca0704cbf7e20ecd170afe283a
S7Dev2 <nkuridza@s7designcreative.com>
2015-06-30 14:19:26 +0200

    port parameter for database

--- app/core/App.php
+++ app/core/App.php
@@ -25,6 +25,7 @@ class App
         $dbParams = array(
             'host'     => DATABASE_HOST,
             'driver'   => DATABASE_DRIVER,
+            'port'     => DATABASE_PORT,
             'user'     => DATABASE_USERNAME,
             'password' => DATABASE_PASSWORD,
             'dbname'   => DATABASE_NAME,
