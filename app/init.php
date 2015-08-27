f4375dfb1a223f5ad5d538e9f4e6247d9c3aef14
S7Dev2 <nkuridza@s7designcreative.com>
2015-08-27 16:58:44 +0200

    gitignore update

--- app/init.php
+++ app/init.php
@@ -2,6 +2,5 @@
 require_once 'config/site_config.php';
 require_once SITE_PATH .'/vendor/autoload.php';
 require_once APP_PATH . 'core/Template.php';
-require_once CONFIG_PATH . '/router.php';
 require_once APP_PATH . 'core/App.php';
 require_once APP_PATH . 'core/Controller.php';
