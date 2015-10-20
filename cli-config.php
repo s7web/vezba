30ada1bb0acc674cb6a0273474ffc4c658ddaddc
s7npaic <npaic@s7designcreative.com>
2015-10-20 12:23:43 +0200

    fixed class call error

--- cli-config.php
+++ cli-config.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$app = new \S7D\Vendor\Routing\Application(__DIR__);
+$app = new \S7D\Core\Routing\Application(__DIR__);
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($app->em);
