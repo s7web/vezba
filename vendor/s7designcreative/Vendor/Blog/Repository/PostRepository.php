f73e7593f8440e1b3a7103e6f71f19f5b1911b4d
S7Dev2 <nkuridza@s7designcreative.com>
2016-01-20 15:36:07 +0100

    function for get last 3 comments

--- vendor/s7designcreative/Vendor/Blog/Repository/PostRepository.php
+++ vendor/s7designcreative/Vendor/Blog/Repository/PostRepository.php
@@ -13,4 +13,19 @@ class PostRepository extends EntityRepository {
 		}
 		return $count->getQuery()->getSingleScalarResult();
 	}
+
+	public function latestComments() {
+		$query = $this->getEntityManager()->getConnection()->prepare(<<<SQL
+SELECT p . *
+FROM post_has_category pc
+LEFT JOIN post p ON ( p.id = pc.post_id )
+WHERE pc.category_id = 12
+ORDER BY p.id DESC
+LIMIT 3
+SQL
+		);
+		$query->execute();
+		return $query->fetchAll();
+
+	}
 }
