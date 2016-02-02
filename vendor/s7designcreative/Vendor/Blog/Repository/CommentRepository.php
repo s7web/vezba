2feb20cc05fabe630ed7232fc7b13b9aefff0451
S7Dev2 <nkuridza@s7designcreative.com>
2016-02-02 17:32:22 +0100

    order as parameter

--- vendor/s7designcreative/Vendor/Blog/Repository/CommentRepository.php
+++ vendor/s7designcreative/Vendor/Blog/Repository/CommentRepository.php
@@ -5,13 +5,13 @@ use Doctrine\ORM\EntityRepository;
 
 class CommentRepository extends EntityRepository {
 
-	public function getByRank($postId, $limit) {
+	public function getByRank($postId, $limit, $order = 'DESC') {
 		return $this->createQueryBuilder('c')
 			->select('c, MAX(c.likes - c.dislikes) as HIDDEN diff')
 			->groupBy('c.id')
 			->where('c.post = :postId')
 			->setParameter('postId', $postId)
-			->orderBy('diff', 'DESC')
+			->orderBy('diff', $order)
 			->setMaxResults($limit)
 			->getQuery()
 			->getResult();
