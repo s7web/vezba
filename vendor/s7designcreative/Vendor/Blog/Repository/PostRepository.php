ee6880541b38ff86854e1ee2b3cd31f2ea6885ba
S7Dev2 <nkuridza@s7designcreative.com>
2016-02-24 17:05:28 +0100

    prevent error

--- vendor/s7designcreative/Vendor/Blog/Repository/PostRepository.php
+++ vendor/s7designcreative/Vendor/Blog/Repository/PostRepository.php
@@ -9,7 +9,7 @@ class PostRepository extends EntityRepository {
 	public function number($filter) {
 		$count = $this->createQueryBuilder('p')->select('COUNT(p.id)');
 		if($filter) {
-			$count->where('p.author = ' . $filter['author']);
+			//$count->where('p.author = ' . $filter['author']);
 		}
 		return $count->getQuery()->getSingleScalarResult();
 	}
