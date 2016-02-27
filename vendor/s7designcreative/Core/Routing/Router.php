7b87ad952a230399af50b663ce069988e3aa661d
S7Dev2 <nkuridza@s7designcreative.com>
2016-02-27 18:54:11 +0100

    absolute path parameter

--- vendor/s7designcreative/Core/Routing/Router.php
+++ vendor/s7designcreative/Core/Routing/Router.php
@@ -10,14 +10,15 @@ class Router {
 		$this->routes[$name] = $route;
 	}
 
-	public function generateUrl($baseUrl, $name, $params) {
+	public function generateUrl($baseUrl, $name, $params, $absolute = false) {
 		if(!isset($this->routes[$name])) {
 			throw new \Exception(sprintf('Route \'%s\' is not defined.', $name));
 		}
 		if(!is_array($params)) {
 			$params = [$params];
 		}
-		return $baseUrl . preg_replace_callback('/\(.*\)/U', function() use (&$params) {
+        $root = $absolute ? $baseUrl : '/';
+		return $root . preg_replace_callback('/\(.*\)/U', function() use (&$params) {
 			return array_shift($params);
 		}, $this->routes[$name]->pattern);
 	}
